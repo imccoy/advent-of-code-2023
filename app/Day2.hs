7aea9a1f9340dd9bd4a555c1010da57f47421843
Iain McCoy <iain@mccoy.id.au>
2021-12-02 22:17:32 +0800

    finished day 2

--- app/Day2.hs
+++ app/Day2.hs
@@ -10,14 +10,18 @@ newtype Depth = Depth Int
 newtype Horizontal = Horizontal Int
   deriving (Show)
 
-data Position = Position !Horizontal !Depth
+newtype Aim = Aim Int
+  deriving (Show)
+
+data Position = Position !Horizontal !Depth !Aim
   deriving (Show)
 
 data Command = Forward !Int | Down !Int | Up !Int
 
-applyCommand (Position (Horizontal h) d) (Forward distance) = Position (Horizontal $ h + distance) d
-applyCommand (Position h (Depth d)) (Down distance) = Position h (Depth $ d + distance)
-applyCommand (Position h (Depth d)) (Up distance) = Position h (Depth $ d - distance)
+
+applyCommand1 (Position (Horizontal h) d a) (Forward distance) = Position (Horizontal $ h + distance) d a
+applyCommand1 (Position h (Depth d) a) (Down distance) = Position h (Depth $ d + distance) a
+applyCommand1 (Position h (Depth d) a) (Up distance) = Position h (Depth $ d - distance) a
 
 readCommand line = case words line of
                      ["forward",amount] -> Forward (read amount)
@@ -27,16 +31,32 @@ readCommand line = case words line of
 input :: IO String
 input = readFile "inputs/day2"
 
-startPosition = Position (Horizontal 0) (Depth 0)
+startPosition = Position (Horizontal 0) (Depth 0) (Aim 0)
 
-navigate :: [Command] -> Position
-navigate = foldl' applyCommand startPosition
+navigate :: (Position -> Command -> Position) -> [Command] -> Position
+navigate applyCommand = foldl' applyCommand startPosition
 
-part1 = do result <- navigate . fmap readCommand . lines <$> input
-           putStrLn . show $ result
-           let (Position (Horizontal h) (Depth d)) = result
-           putStrLn . show $ h * d
-part2 = putStrLn "part2"
+
+run applyCommand = do
+  result <- navigate applyCommand . fmap readCommand . lines <$> input
+  putStrLn . show $ result
+  let (Position (Horizontal h) (Depth d) _) = result
+  putStrLn . show $ h * d
+
+
+part1 = run applyCommand1
+
+applyCommand2 (Position (Horizontal h) (Depth d) (Aim a)) (Forward distance) =
+  Position
+    (Horizontal $ h + distance)
+    (Depth $ d + a * distance)
+    (Aim a)
+applyCommand2 (Position h d (Aim a)) (Down distance) = Position h d (Aim $ a + distance)
+applyCommand2 (Position h d (Aim a)) (Up distance) = Position h d (Aim $ a - distance)
+
+
+
+part2 = run applyCommand2
 
 day2 Part1 _ = part1
 day2 Part2 _ = part2
