180c2881960df09990f4ff0bdd7f27a0d76c6902
Iain McCoy <iain@mccoy.id.au>
2021-12-04 12:18:21 +0800

    silly functional adventures

--- app/Day3.hs
+++ app/Day3.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE LambdaCase, DeriveFunctor #-}
 module Day3 where
 
 import Data.Bits
 import Data.List (foldl')
-import Debug.Trace
+import Data.Maybe (mapMaybe)
 
 import Part (Part (Part1, Part2))
 import Day3.Common
@@ -33,10 +34,49 @@ searchFor criteria nums0 = go $ zip nums0 nums0
                   matchingRows = filter ((== digitToMatch) . head . fst) nums
                in go $ map (\(remainingBits, allBits) -> (tail remainingBits, allBits)) matchingRows
 
+
+
+data SearchEntry a = SearchHere a | SearchSkip !Int
+  deriving (Functor)
+
+searchEntryMaybe (SearchHere a) = Just a
+searchEntryMaybe (SearchSkip _) = Nothing
+
+searchFor' :: (CountPair -> Bit) -> [[Bit]] -> Int
+searchFor' criteria nums0 = toInt . go . ((SearchSkip 0):) . map SearchHere $ nums0
+  where
+    go :: [SearchEntry [Bit]] -> [Bit]
+    go [] = error "we have gone too far"
+    go [SearchSkip _] = error "we have gone too far"
+    go [SearchSkip n, SearchHere _, SearchSkip _] = head . drop n $ nums0
+    go nums = let splits = fmap (\case
+                                   (SearchSkip n) -> SearchSkip n
+                                   (SearchHere []) -> error "empty SearchHere"
+                                   (SearchHere (x:xs)) -> SearchHere (x, xs)
+                                )
+                                nums
+                  leadingDigits = mapMaybe (fmap fst . searchEntryMaybe) splits
+                  counts = foldl' (<>) mempty . map bitToCountPair $ leadingDigits
+                  digitToMatch = criteria counts
+               in go . mergeSkips . map (\case
+                                           SearchHere (x, xs) -> if x == digitToMatch
+                                                                   then SearchHere xs
+                                                                   else SearchSkip 1
+                                           SearchSkip n -> SearchSkip n
+                                        ) $ splits
+    mergeSkips :: [SearchEntry a] -> [SearchEntry a]
+    mergeSkips ((SearchSkip n):((SearchSkip m):rest)) = mergeSkips $ (SearchSkip (n+m)):(mergeSkips rest)
+    mergeSkips (here:rest) = here:(mergeSkips rest)
+    mergeSkips [] = []
+
+
 part2 args = do nums <- input args
                 let oxygen = searchFor mostCommonBit nums
                 let co2 = searchFor leastCommonBit nums
                 putStrLn $ show (oxygen, co2, oxygen * co2)
+                let oxygen' = searchFor' mostCommonBit nums
+                let co2' = searchFor' leastCommonBit nums
+                putStrLn $ show (oxygen', co2', oxygen' * co2')
 
 day3 Part1 _ = do part1
                   Vectory.part1
